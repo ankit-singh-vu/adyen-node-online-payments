e54960c58efd624fc83968811386897ce6cef520
Ankit Singh <ankitsinggggg@gmail.com>
2023-11-27 12:46:23 +0000

    obj

--- checkout-example-advanced/index.js
+++ checkout-example-advanced/index.js
@@ -72,45 +72,49 @@ app.post("/api/initiatePayment", async (req, res) => {
     // const isHttps = req.connection.encrypted;
     const protocol = req.socket.encrypted? 'https' : 'http';    
     // Ideally the data passed here should be computed based on business logic
-    const response = await checkout.PaymentsApi.payments({
-      amount: { currency, value: 1000 }, // value is 10€ in minor units
-      reference: orderRef, // required
-      merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT, // required
-      channel: "Web", // required
-      additionalData: {
-        // required for 3ds2 native flow
-        allow3DS2: true,
-      },
-      origin: `${protocol}://${localhost}`, // required for 3ds2 native flow
-      browserInfo: req.body.browserInfo, // required for 3ds2
-      shopperIP, // required by some issuers for 3ds2
-      returnUrl: `${protocol}://${localhost}/api/handleShopperRedirect?orderRef=${orderRef}`, // required for 3ds2 redirect flow
-      // special handling for boleto
-      paymentMethod: req.body.paymentMethod.type.includes("boleto")
-        ? {
-            type: "boletobancario_santander",
-          }
-        : req.body.paymentMethod,
-      // Below fields are required for Boleto:
-      socialSecurityNumber: req.body.socialSecurityNumber,
-      shopperName: req.body.shopperName,
-      billingAddress:
-        typeof req.body.billingAddress === "undefined" || Object.keys(req.body.billingAddress).length === 0
-          ? null
-          : req.body.billingAddress,
-      deliveryDate: new Date("2017-07-17T13:42:40.428+01:00"),
-      shopperStatement: "Aceitar o pagamento até 15 dias após o vencimento.Não cobrar juros. Não aceitar o pagamento com cheque",
-      // Below fields are required for Klarna:
-      countryCode: req.body.paymentMethod.type.includes("klarna") ? "DE" : null,
-      shopperReference: "12345",
-      shopperEmail: "youremail@email.com",
-      shopperLocale: "en_US",
-      lineItems: [
-        {quantity: 1, amountIncludingTax: 5000 , description: "Sunglasses"},
-        {quantity: 1, amountIncludingTax: 5000 , description: "Headphones"}
-      ],
-    });
 
+let myobj= {
+  amount: { currency, value: 0 }, // value is 10€ in minor units
+  reference: orderRef, // required
+  merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT, // required
+  channel: "Web", // required
+  additionalData: {
+    // required for 3ds2 native flow
+    allow3DS2: true,
+  },
+  origin: `${protocol}://${localhost}`, // required for 3ds2 native flow
+  browserInfo: req.body.browserInfo, // required for 3ds2
+  shopperIP, // required by some issuers for 3ds2
+  returnUrl: `${protocol}://${localhost}/api/handleShopperRedirect?orderRef=${orderRef}`, // required for 3ds2 redirect flow
+  // special handling for boleto
+  paymentMethod: req.body.paymentMethod.type.includes("boleto")
+    ? {
+        type: "boletobancario_santander",
+      }
+    : req.body.paymentMethod,
+  // Below fields are required for Boleto:
+  socialSecurityNumber: req.body.socialSecurityNumber,
+  shopperName: req.body.shopperName,
+  billingAddress:
+    typeof req.body.billingAddress === "undefined" || Object.keys(req.body.billingAddress).length === 0
+      ? null
+      : req.body.billingAddress,
+  deliveryDate: new Date("2017-07-17T13:42:40.428+01:00"),
+  shopperStatement: "Aceitar o pagamento até 15 dias após o vencimento.Não cobrar juros. Não aceitar o pagamento com cheque",
+  // Below fields are required for Klarna:
+  countryCode: req.body.paymentMethod.type.includes("klarna") ? "DE" : null,
+  shopperReference: "12345",
+  shopperEmail: "youremail@email.com",
+  shopperLocale: "en_US",
+  lineItems: [
+    {quantity: 1, amountIncludingTax: 5000 , description: "Sunglasses"},
+    {quantity: 1, amountIncludingTax: 5000 , description: "Headphones"}
+  ],
+};
+    console.log("Our myobj :"+ JSON.stringify(myobj))
+    const response = await checkout.PaymentsApi.payments(myobj);
+
+    console.log("Our Response :"+ JSON.stringify(response))
     res.json(response);
   } catch (err) {
     console.error(`Error: ${err.message}, error code: ${err.errorCode}`);
